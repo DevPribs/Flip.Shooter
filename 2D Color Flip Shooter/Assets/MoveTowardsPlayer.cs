feedc990b44c92771f6f856792ef011d3b98fc9e
DevPribs <purpribs@hotmail.com>
2016-06-14 14:28:06 -0700

    Added current project

---
+++ 2D Color Flip Shooter/Assets/MoveTowardsPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsPlayer : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        //Vector2 playerPosition = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, 0.1F);
+	}
+}
---
+++ 2D Color Flip Shooter/Assets/ShootingScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingScript : MonoBehaviour {
+
+    GameObject bullet;
+	// Use this for initialization
+	void Start () {
+	
+        bullet = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
