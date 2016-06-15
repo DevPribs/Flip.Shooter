e7adceac3ce169e8a84d9050f2bbdbb0dedc659a
DevPribs <purpribs@hotmail.com>
2016-06-15 16:01:55 -0700

    Added touch support Added ability for player icon to turn towards current touch Added ability to fire bullets but they currently go the wrong direction

--- 2D Color Flip Shooter/Assets/ShootingScript.cs
+++ 2D Color Flip Shooter/Assets/ShootingScript.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class ShootingScript : MonoBehaviour {
 
     GameObject bulletItem;
+    Vector2 currentPosition;
+    Vector3 pointingDirection;
+    float angle;
+    Quaternion rotation;
 	// Use this for initialization
 	void Start () {
 
@@ -12,13 +16,42 @@ public class ShootingScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetMouseButtonDown(0))
+
+        int numberTouches = Input.touchCount;
+
+        if(numberTouches > 0)
         {
-            GameObject bulletObject = Instantiate(bulletItem) as GameObject;
-            bulletObject.transform.position = transform.position;
-            Rigidbody2D rigidbody = bulletObject.GetComponent<Rigidbody2D>();
-            rigidbody.AddForce(rigidbody.transform.forward * 0.1f);
+            Touch touch = Input.GetTouch(0);
+            currentPosition = touch.position;
+            currentPosition = new Vector2(currentPosition.x - Screen.width / 2, currentPosition.y - Screen.height / 2);
+            angle = Mathf.Atan(currentPosition.y / currentPosition.x);
+            if(currentPosition.x < 0)
+            {
+                angle += Mathf.PI;
+            }
+            transform.rotation = Quaternion.AngleAxis((angle*180)/Mathf.PI, Vector3.forward); 
+
+            //Vector3 lastPositionV3 = new Vector3(currentPosition.x, currentPosition.y);
+            //pointingDirection = (lastPositionV3 - transform.position).normalized;
+            //rotation = Quaternion.LookRotation(pointingDirection);
+            //transform.rotation = rotation;
+
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                GameObject bulletObject = Instantiate(bulletItem) as GameObject;
+                bulletObject.transform.position = transform.position;
+                Rigidbody2D rigidbody = bulletObject.GetComponent<Rigidbody2D>();
+                rigidbody.AddForce(rigidbody.transform.eulerAngles * 0.1f);
+            }
         }
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //    GameObject bulletObject = Instantiate(bulletItem) as GameObject;
+        //    bulletObject.transform.position = transform.position;
+        //    Rigidbody2D rigidbody = bulletObject.GetComponent<Rigidbody2D>();
+        //    rigidbody.AddForce(rigidbody.transform.forward * 0.1f);
+        //}
 	
 	}
 }
---
+++ 2D Color Flip Shooter/Assets/TestCords.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestCords : MonoBehaviour {
+
+    Vector2 currentPosition;
+    Text text;
+    Text text2;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        text = GameObject.Find("Cords").GetComponent<Text>();
+        text2 = GameObject.Find("Cords2").GetComponent<Text>();
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int numberTouches = Input.touchCount;
+
+        if (numberTouches > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            currentPosition = touch.position;
+            text.text = (int)currentPosition.x + ", " + (int)currentPosition.y;
+            text2.text = (int)(currentPosition.x - Screen.width/2) + ", " + (int)(currentPosition.y - Screen.height/2);
+
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                
+            }
+        }
+    }
+}
