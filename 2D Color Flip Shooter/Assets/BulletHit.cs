760e2d5510aadf22a4749609f80088b196ede44b
DevPribs <purpribs@hotmail.com>
2016-06-20 15:50:36 -0700

    Changed the location the score was calculated Added a score multiplier Added a score multiplier bar

--- 2D Color Flip Shooter/Assets/BulletHit.cs
+++ 2D Color Flip Shooter/Assets/BulletHit.cs
@@ -4,13 +4,9 @@ using System.Collections;
 
 public class BulletHit : MonoBehaviour {
 
-    Text text;
-    int score = 0;
-
 	// Use this for initialization
 	void Start () {
 
-        text = GameObject.Find("Score").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
@@ -23,8 +19,7 @@ public class BulletHit : MonoBehaviour {
         if(col.gameObject.name == "Enemy")
         {
             Destroy(col.gameObject);
-            score += 10;
-            text.text = "" + score;
+            GameObject.Find("GameManager").GetComponent<Score>().AddScore(10);
             Destroy(this.gameObject);
         }
     }
---
+++ 2D Color Flip Shooter/Assets/Score.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    Text text;
+    int score = 0;
+    double multiplier = 1;
+    Slider multiplierSlide;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        text = GameObject.Find("Score").GetComponent<Text>();
+        multiplierSlide = GameObject.Find("MultiplierSlide").GetComponent<Slider>();
+        multiplierSlide.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddScore(int points)
+    {
+        score += (int)((double)points * multiplier);
+        text.text = "" + score;
+        multiplierSlide.value += 10;
+        if(multiplierSlide.value == 100)
+        {
+            multiplierSlide.value = 0;
+            multiplier++;
+        }
+    }
+}
