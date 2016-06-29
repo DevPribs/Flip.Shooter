69de9462d5df8f8272b37e506aa7a926d38c29f0
DevPribs <purpribs@hotmail.com>
2016-06-29 13:53:56 -0700

    Color sliders and inputs will update eachother

---
+++ 2D Color Flip Shooter/Assets/UpdateDataNumber.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateDataNumber : MonoBehaviour {
+
+	public void updateDataNumber(Slider slider)
+    {
+        this.GetComponent<InputField>().text = "" + slider.value;
+    } 
+}
---
+++ 2D Color Flip Shooter/Assets/UpdateSliderNumber.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateSliderNumber : MonoBehaviour {
+
+	public void updateSliderNumber(InputField inputfield)
+    {
+        int x;
+        bool number = int.TryParse(inputfield.text,out x);
+        if(number)
+        {
+            this.GetComponent<Slider>().value = x;
+            if (x > 255)
+            {
+                inputfield.text = "" + 255;
+            }
+        }
+        
+    }
+}
